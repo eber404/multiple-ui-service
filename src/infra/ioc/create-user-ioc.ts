bed5dd947519947ca764d7d3a86a7b9cae2e6020
eber404 <bodacamargo@gmail.com>
2023-01-13 02:20:57 +0000

    refactor: add path alias to controller import on ioc

--- src/infra/ioc/create-user-ioc.ts
+++ src/infra/ioc/create-user-ioc.ts
@@ -1,6 +1,6 @@
 import { CreateUserController } from "@/presentation/controllers/create-user-controller.ts";
 import { CreateUserUseCase } from "@/application/create-user-usecase.ts";
-import { InMemoryCreateUserRepository } from "../repositories/in-memory-create-user-repository.ts";
+import { InMemoryCreateUserRepository } from "@/infra/repositories/in-memory-create-user-repository.ts";
 
 const createUserRepository = new InMemoryCreateUserRepository();
 const createUserUseCase = new CreateUserUseCase(createUserRepository);
